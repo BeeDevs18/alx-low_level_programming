89f596f3f80f63111af4afcae0a3d874059b7efe
onwabless <onwablessing6@gmail.com>
2023-02-16 16:16:00 -0800

    message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -is the entry point for the program.
+ *
+ * Return:zero if no error, Non-zero value  if errors
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
